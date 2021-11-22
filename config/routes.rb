c8e71abfb44bb6f256974d99e5b7499b559e13f1
Tolibjon <tolibjohn@gmail.com>
2021-11-22 21:09:53 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "foods#index"
+
+  # resources :foods, only: %i[index show]
+ 
+  resources :users, only: %i[index show] do 
+    resources :foods, only: %i[index show]
+    resources :recipes, only: %i[index show]
+    resources :inventories, only: %i[index show]
+  end
 end
