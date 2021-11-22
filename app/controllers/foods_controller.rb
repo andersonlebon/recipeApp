e59990490f342b7ded95796fa4b1e52e74669dec
Tolibjon <tolibjohn@gmail.com>
2021-11-22 21:08:24 +0500

    Create actions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,31 @@
 class FoodsController < ApplicationController
+  # before_action :authenticate_user!
+
+  def index
+    @foods = Food.all
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def new
+    @food = Food.new
+  end
+   
+  def create
+    @food = Food.new(food_params)
+
+    if @food.save 
+      redirect_to @Food
+    else
+      render :new
+    end
+  end
+
+private
+
+def food_params
+  params.require(:food).permit(:name, :measurement_unit, :price)
+end
 end
