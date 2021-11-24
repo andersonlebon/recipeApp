b8b0bb4226b53beb97eebc98ca56f32a4efaf1fe
Tolibjon <tolibjohn@gmail.com>
2021-11-24 09:22:17 +0500

    Add create action for the inventories

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -5,8 +5,24 @@ class InventoriesController < ApplicationController
     @inventories = Inventory.all
   end
 
+  def new
+    @inventory = Inventory.new  
+  end
 
+  def create
+    @inventory = Inventory.new(inventory_params)
+    @inventory.user = current_user
 
+    if @inventory.save
+      redirect_to user_inventories_path, notice: 'Inventory added succesfully'
+    else
+      render :new
+    end
+  end
   
+private
 
+def inventory_params
+  params.require(:inventory).permit(:name, :description) 
+end
 end
