b64b31634e90f3d188058c5ca0083ba9fe917256
Caleb Anderson <buyananderson@gmail.com>
2021-11-24 20:31:25 +0200

    Write test for the inventory model

---
+++ spec/models/inventory_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+# Test for inventory model
+RSpec.describe Inventory, type: :model do
+  # Create a new inventory
+  before(:each) do
+        @user = User.new(name: 'Test User', email: 'exemple@gmail.com', password: 'password')
+        @inventory = Inventory.new(name: 'Test Inventory', user: @user)
+    end
+
+    # should belong to user
+    it 'belongs to a user' do
+        expect(@inventory.user).to eq(@user)
+    end
+
+    # should have a name
+    it 'has a name' do
+        expect(@inventory.name).to eq('Test Inventory')
+    end
+
+    # should have many  inventory_foods\
+    it 'has many inventory_foods' do
+        expect(@inventory).to respond_to(:inventory_foods)
+    end
+
+end
+    
