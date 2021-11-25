972e214b7b73eae7ae9c9a1e7a33959f3be0a830
Sylvestre <pasytchangwa@gmail.com>
2021-11-25 15:45:55 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,14 @@ Rails.application.routes.draw do
   # resources :foods, only: %i[index show]
  
   resources :users, only: %i[index show] do 
-    resources :foods, only: %i[index show]
-    resources :recipes, only: %i[index show]
+    resources :foods, only: %i[index show new]
     resources :inventories, only: %i[index show]
+    resources :recipe_foods, only: %i[update, edit]
+    resources :recipes, only: %i[index show new create destroy update] do
+      resources :recipe_foods, only: %i[new create destroy]
+    end
   end
+  
+  get 'public_recipes', to: 'recipes#public_recipes'
+  get 'general_shopping_list', to: 'general_shopping_lists#index'
 end
